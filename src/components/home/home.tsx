0ea3a0b597193214605765e52cb96f061e70cbd9
Matiase <matias.f.espinoza@gmail.com>
2020-12-01 12:58:26 -0300

    refactor home: img Argentina

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Quotation from '../quotation/quotation'
+import React from "react";
+import Quotation from "../quotation/quotation";
 
 export default function Home() {
   return (
     <div>
-      <div><h4>Cotizacion de moneda: </h4></div>
-      <div><h3>1 peso argentino</h3></div>
+      <h4>Cotizacion de moneda: </h4>
+      <h3>1 peso argentino</h3>
+      <img
+        height={"50vh"}
+        alt={"Argentina"}
+        src={"/img/map-ambit-ars.png"}
+      ></img>
       <Quotation></Quotation>
     </div>
-  )
+  );
 }
