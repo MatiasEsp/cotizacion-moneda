d2b4962bbdbf7475f4826e5a50db62fedcb3da2f
Matiase <matias.f.espinoza@gmail.com>
2020-12-01 13:53:53 -0300

    bug process.env.NODE_ENV: hardcode apiServer

--- src/services/apis.ts
+++ src/services/apis.ts
@@ -1,4 +1,5 @@
-const URL_SERVER = process.env.NODE_ENV === 'production' ? 'https://api-cotizacion-moneda.herokuapp.com' : 'http://localhost:8080';
+// const URL_SERVER = process.env.NODE_ENV === 'production' ? 'https://api-cotizacion-moneda.herokuapp.com' : 'http://localhost:8080';
+const URL_SERVER = 'https://api-cotizacion-moneda.herokuapp.com';
 
 export const APIs = {
   QUOTATION: `${URL_SERVER}/cotizacion`
