9af60dbafb09db1bdae0a199d6336364661cfd39
Matiase <matias.f.espinoza@gmail.com>
2020-11-30 23:36:07 -0300

    create component Quotation

---
+++ src/components/quotation/quotation.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { valueTypeMoney } from "../../services/quotation.service";
+import Coin from "../coin/coin";
+
+class Quotation extends Component<IProps, IState> {
+  render() {
+    return (
+      <div className={'row'}>
+      </div>
+    )
+  }
+}
+
+export default Quotation;
+
+interface IProps {
+}
+
+interface IState {
+}
