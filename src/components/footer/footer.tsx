f9ecd55c55fa51e3022fe37e2f2c7c283386a731
Matiase <matias.f.espinoza@gmail.com>
2020-12-01 12:58:00 -0300

    refactor footer: link linkedin

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div style={{
-      position: 'fixed', backgroundColor: '#084E5F', margin: '0',
-      left: 0, bottom: 0,
-      height: '100px', width: '100%', alignContent: 'center', alignItems: 'center'
-    }} className={'page-footer'} >
+    <div
+      style={{
+        position: "fixed",
+        backgroundColor: "#D6EAF8",
+        margin: "0",
+        left: 0,
+        bottom: 0,
+        height: "100px",
+        width: "100%",
+        alignContent: "center",
+        alignItems: "center",
+      }}
+      className={"page-footer"}
+    >
       <h4>
-        <p style={{ color: '#FFFFFF' }}>{`Copyright © - `}
-          <a rel="stylesheet" href={'https://www.linkedin.com/in/matias-espinoza-09a736187/'} target="_blank" >
+        <p style={{ color: "#000000" }}>
+          {`Copyright © - `}
+          <a
+            rel="stylesheet"
+            href={"https://www.linkedin.com/in/matias-espinoza-09a736187/"}
+            target="_blank"
+          >
             Espinoza, Matias
-        </a>
+          <img
+            height={"30vh"}
+            alt={"Argentina"}
+            src={"/img/linkedin.png"}
+          ></img>
+          </a>
         </p>
       </h4>
     </div>
-  )
+  );
 }
